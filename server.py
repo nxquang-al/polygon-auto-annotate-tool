f8b408a31b03b2b63c0134783f852b85aff295e0
ngxquang <nq23092016@gmail.com>
2023-06-01 14:25:04 +0700

    Change port

--- server.py
+++ server.py
@@ -141,5 +141,5 @@ def exclude():
 if __name__ == "__main__":
     demo = VisualizationDemo()
 
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port, debug=True)
